3bd1d102aa8e9eafe9efd6ce74ef8b64af32c46e
Simon Rothman <srothman@mit.edu>
2024-02-20 14:06:17 -0500

    Start work on reading res3

--- reading/readEEC.py
+++ reading/readEEC.py
@@ -6,10 +6,18 @@ from util.util import unflatMatrix, unflatVector
 def getNProj(x, name):
     return x[name+"BK"].nproj
 
+def getNres3(x, name):
+    return x[name+"BK"].nres3
+
 def getProj(x, name, which):
     wts = x[name+'proj'][which]
     nproj = getNProj(x, name)
     return unflatVector(wts, nproj)
+
+def getRes3(x, name):
+    wts = x[name+'res3'].value
+    nres3 = getNres3(x, name)
+    return unflatVector(wts, nres3)
 
 def getJetIdx(x, name):
     return x[name+"BK"].iJet
@@ -27,3 +35,11 @@ def getTransferP(x, name, which):
     nmat = nproj*nproj
 
     return unflatMatrix(vals, nproj, nproj)
+
+def getTransferRes3(x, name):
+    vals = x[name+'res3'].value
+
+    nres3 = getNres3(x, name)
+    nmat = nres3*nres3
+
+    return unflatMatrix(vals, nres3, nres3)
