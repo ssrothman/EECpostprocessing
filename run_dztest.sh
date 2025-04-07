b80a649d6374eae807db5b9c749a0b71c9434525
Simon Rothman <srothman@mit.edu>
2025-04-07 08:48:01 -0400

    rm old .sh

--- run_dztest.sh
+++
@@ -1,2 +0,0 @@
-python process.py DYJetsToLL_nodz EECproj ak8 inclusive --force --samplelist 06_28_2024 --manualcov  --nfile 1000
-python process.py DYJetsToLL EECproj ak8 inclusive --force --samplelist 06_28_2024 --manualcov --nfile 1000
