f62115a57692108a12ba0e6b507bf66949940ec7
Simon Rothman <srothman@mit.edu>
2025-06-12 17:53:51 -0400

    Fixed dipole and triangle binners

--- binning/EECres4dipole.py
+++ binning/EECres4dipole.py
@@ -3,72 +3,97 @@ import awkward as ak
 import hist
 from binning.util import *
 from time import time
+import os.path
 
 from .EECgeneric import EECgenericBinner
 
 class EECres4dipoleBinner(EECgenericBinner):
-    def __init__(self, config,
-                 manualcov,
-                 poissonbootstrap, 
-                 skipBtag,
-                 statsplit,
-                 sepPt):
-        super(EECres4dipoleBinner, self).__init__(config,
-                                            manualcov, 
-                                            poissonbootstrap,
-                                            skipBtag,
-                                            statsplit, 
-                                            sepPt)
+    def __init__(self, *args, **kwargs):
+        super(EECres4dipoleBinner, self).__init__(*args, **kwargs)
 
-    def binAll(self, readers, mask, evtMask, wt):
+    def binAll(self, readers, mask, evtMask, wtVars, basepath):
         result = {}
         result['reco'] = self.binObserved(
                 readers.rRecoEEC.res4dipole,
+                readers.rRecoEEC.ptDenom, 4,
                 readers.rRecoJet,
                 readers.rRecoEEC.iJet,
                 readers.rRecoEEC.iReco,
                 readers.eventIdx,
-                mask, wt)
+                mask, wtVars,
+                os.path.join(basepath, 'reco'),
+                isRes=True)
         
-        result['gen'] = self.binObserved(
-                readers.rGenEEC.res4dipole,
-                readers.rGenJet,
-                readers.rGenEEC.iJet,
-                readers.rGenEEC.iReco,
-                readers.eventIdx,
-                mask, wt)
+        if self.isMC:
+            result['gen'] = self.binObserved(
+                    readers.rGenEEC.res4dipole,
+                    readers.rGenEEC.ptDenom, 4,
+                    readers.rGenJet,
+                    readers.rGenEEC.iJet,
+                    readers.rGenEEC.iReco,
+                    readers.eventIdx,
+                    mask, wtVars,
+                    os.path.join(basepath, 'gen'),
+                    isRes=True)
 
-        result['unmatchedReco'] = self.binObserved(
-                readers.rUnmatchedRecoEEC.res4dipole,
+            result['unmatchedReco'] = self.binObserved(
+                    readers.rUnmatchedRecoEEC.res4dipole,
+                    readers.rUnmatchedRecoEEC.ptDenom, 4,
+                    readers.rRecoJet,
+                    readers.rUnmatchedRecoEEC.iJet,
+                    readers.rUnmatchedRecoEEC.iReco,
+                    readers.eventIdx,
+                    mask, wtVars,
+                    os.path.join(basepath, 'unmatchedReco'),
+                    isRes=True)
+
+            result['unmatchedGen'] = self.binObserved(
+                    readers.rUnmatchedGenEEC.res4dipole,
+                    readers.rUnmatchedGenEEC.ptDenom, 4,
+                    readers.rGenJet,
+                    readers.rUnmatchedGenEEC.iJet,
+                    readers.rUnmatchedGenEEC.iReco,
+                    readers.eventIdx,
+                    mask, wtVars,
+                    os.path.join(basepath, 'unmatchedGen'),
+                    isRes=True)
+
+            result['untransferedReco'] = self.binObserved(
+                    readers.rUntransferedRecoEEC.res4dipole,
+                    readers.rUntransferedRecoEEC.ptDenom, 4,
+                    readers.rRecoJet,
+                    readers.rUntransferedRecoEEC.iReco,
+                    readers.rUntransferedRecoEEC.iReco,
+                    readers.eventIdx,
+                    mask, wtVars,
+                    os.path.join(basepath, 'untransferedReco'),
+                    isRes=True)
+
+            result['untransferedGen'] = self.binObserved(
+                    readers.rUntransferedGenEEC.res4dipole,
+                    readers.rUntransferedGenEEC.ptDenom, 4,
+                    readers.rGenJet,
+                    readers.rUntransferedGenEEC.iJet,
+                    readers.rUntransferedGenEEC.iReco,
+                    readers.eventIdx,
+                    mask, wtVars,
+                    os.path.join(basepath, 'untransferedGen'),
+                    isRes=True)
+
+            result['transfer'] = self.binTransfer(
+                readers.rTransfer.res4dipole,
+                readers.rTransfer.ptDenomReco,
+                readers.rTransfer.ptDenomGen,
+                4,
+                readers.rGenJet,
                 readers.rRecoJet,
-                readers.rUnmatchedRecoEEC.iJet,
-                readers.rUnmatchedRecoEEC.iReco,
+                readers.rTransfer.iGen,
+                readers.rTransfer.iReco,
                 readers.eventIdx,
-                mask, wt)
-
-        result['unmatchedGen'] = self.binObserved(
-                readers.rUnmatchedGenEEC.res4dipole,
-                readers.rGenJet,
-                readers.rUnmatchedGenEEC.iJet,
-                readers.rUnmatchedGenEEC.iReco,
-                readers.eventIdx,
-                mask, wt)
-
-        result['untransferedReco'] = self.binObserved(
-                readers.rUntransferedRecoEEC.res4dipole,
-                readers.rRecoJet,
-                readers.rUntransferedRecoEEC.iReco,
-                readers.rUntransferedRecoEEC.iReco,
-                readers.eventIdx,
-                mask, wt)
-
-        result['untransferedGen'] = self.binObserved(
-                readers.rUntransferedGenEEC.res4dipole,
-                readers.rGenJet,
-                readers.rUntransferedGenEEC.iJet,
-                readers.rUntransferedGenEEC.iReco,
-                readers.eventIdx,
-                mask, wt)
+                mask, wtVars,
+                os.path.join(basepath, 'transfer'),
+                isRes=True
+            )
 
         return result
     
