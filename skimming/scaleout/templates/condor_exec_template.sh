59cb7c401ee4c74a792ddefbd4cf5b2de4c5f3ce
Simon Rothman <srothman@mit.edu>
2026-01-01 16:30:24 -0500

    condor submission which actually works???

--- skimming/scaleout/templates/condor_exec_template.sh
+++ skimming/scaleout/templates/condor_exec_template.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-set -euxo pipefail # Exit with nonzero exit code if anything fails 
+set -e # Exit with nonzero exit code if anything fails 
 
 JOB_INDEX=$1
 echo "Starting job $JOB_INDEX"
 echo "Running on host $(hostname)"
 
-git clone git@github.com:ssrothman/EECpostprocessing.git -b refactor
-cd postprocessing/
+git clone https://github.com/ssrothman/EECpostprocessing.git -b refactor --depth 1 --no-tags --recurse-submodules --shallow-submodules --single-branch
+cd EECpostprocessing/
 source env.sh
-git submodule update --init --recursive
 
 cd ../
 
