84c42c41a1b0f59bda7366d2ff73a87d5e54186e
Simon Rothman <srothman@mit.edu>
2026-01-11 12:45:11 -0500

    Clean up more legacy code

--- binning/old/sacct_all.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-./get_slurm_job_ids.sh
-sacct -o "JobID%16, MaxRSS, TotalCPU, Elapsed, State, ExitCode" --units G -j $(cat slurm_job_ids.txt | awk '{print $1".batch"}' | paste -s -d,)
--- binning/old/seff_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in $(cat slurm_job_ids.txt); do
-    echo "Job ID: $i"
-    seff $i
-    echo "----------------------------------------"
-done
