e6e4d3e0fb925007e9c1b6ef9487dcac664c38a8
Simon Rothman <srothman@mit.edu>
2026-01-11 10:27:03 -0500

    clean up signatures

--- binning/main.py
+++ binning/main.py
@@ -1,7 +1,9 @@
+from typing import Sequence
 import hist
 from tqdm import tqdm
 import numpy as np
 import directcov
+import pyarrow.dataset as ds
 
 def build_transfer_hist(gencfg, recocfg):
     axes = []
@@ -46,8 +48,7 @@ def build_transfer_hist(gencfg, recocfg):
         else:
             raise ValueError(f"Unknown axis type: {axis_cfg['type']}") 
     H = hist.Hist(*axes, storage=hist.storage.Weight())
-    names = list(H.axes.name)
-    return H, names
+    return H
 
 def build_hist(cfg):
     axes = []
@@ -73,18 +74,17 @@ def build_hist(cfg):
             raise ValueError(f"Unknown axis type: {axis_cfg['type']}")
         
     H = hist.Hist(*axes, storage=hist.storage.Weight())
-    names = list(H.axes.name)
-    return H, names
+    return H
 
 def get(batch, name):
     return batch[name].to_numpy(zero_copy_only=False)
 
-def build_iterator(ds, names, weightname, itemwt):
-    columns = names + [weightname, 'event_id']
+def build_iterator(dset : ds.Dataset, names : Sequence[str], weightname: str, itemwt: str | None):
+    columns = list(names) + [weightname, 'event_id']
     if itemwt is not None:
         columns.append(itemwt)
 
-    return tqdm(ds.to_batches(
+    return tqdm(dset.to_batches(
         columns = columns,
         batch_readahead = 2,
         fragment_readahead = 2,
@@ -92,9 +92,9 @@ def build_iterator(ds, names, weightname, itemwt):
         batch_size = 1<<20
     ))
 
-def fill_cov(H, names, ds, weightname, itemwt=None):
-    iterator = build_iterator(ds, names, weightname, itemwt)
-    total_rows = ds.count_rows()
+def fill_cov(H, dset : ds.Dataset, weightname: str, itemwt: str | None = None):
+    iterator = build_iterator(dset, H.axes.name, weightname, itemwt)
+    total_rows = dset.count_rows()
     rows_so_far = 0
 
     shape = [ax.extent for ax in H.axes]
@@ -105,11 +105,11 @@ def fill_cov(H, names, ds, weightname, itemwt=None):
         iterator.set_description("Rows: %g%%" % (rows_so_far / total_rows * 100))
 
         indices = [
-            H.axes[name].index(get(batch, name)) for name in names
+            H.axes[name].index(get(batch, name)) for name in H.axes.name
         ]
         indices = np.stack(indices, axis=1) # pyright: ignore[reportArgumentType, reportCallIssue]
         
-        for i, name in enumerate(names):
+        for i, name in enumerate(H.axes.name):
             if H.axes[name].traits.underflow:
                 indices[:,i] += 1
 
@@ -122,9 +122,9 @@ def fill_cov(H, names, ds, weightname, itemwt=None):
 
     return np.array(cov, copy=True)
 
-def fill_hist(H, names, ds, weightname, itemwt=None):
-    iterator = build_iterator(ds, names, weightname, itemwt)
-    total_rows = ds.count_rows()
+def fill_hist(H : hist.Hist, dset : ds.Dataset, weightname : str, itemwt : str | None = None):
+    iterator = build_iterator(dset, H.axes.name, weightname, itemwt)
+    total_rows = dset.count_rows()
     rows_so_far = 0
 
     for batch in iterator:
@@ -132,7 +132,7 @@ def fill_hist(H, names, ds, weightname, itemwt=None):
         iterator.set_description("Rows: %g%%" % (rows_so_far / total_rows * 100))
 
         H.fill(
-            **{name: get(batch, name) for name in names},
+            **{name: get(batch, name) for name in H.axes.name},
             weight = get(batch, weightname)
         )
 
--- binning/test.py
+++ binning/test.py
@@ -10,19 +10,19 @@ dspath_transfer = '/ceph/submit/data/group/cms/store/user/srothman/EEC_v2/BasicC
 from binning.main import build_hist, build_transfer_hist, fill_hist, fill_cov
 import pyarrow.dataset as ds
 
-Hgen, names_gen = build_hist(binning_cfg['gen'])
+Hgen = build_hist(binning_cfg['gen'])
 ds_gen = ds.dataset(dspath_gen, format="parquet")
-Hgen = fill_hist(Hgen, names_gen, ds_gen, 'wt_nominal', 'wt')
-covgen = fill_cov(Hgen, names_gen, ds_gen, 'wt_nominal', 'wt')
+Hgen = fill_hist(Hgen, ds_gen, 'wt_nominal', 'wt')
+covgen = fill_cov(Hgen, ds_gen, 'wt_nominal', 'wt')
 
 
-Hreco, names_reco = build_hist(binning_cfg['reco'])
+Hreco = build_hist(binning_cfg['reco'])
 ds_reco = ds.dataset(dspath_reco, format="parquet")
-Hreco = fill_hist(Hreco, names_reco, ds_reco, 'wt_nominal', 'wt')
-covreco = fill_cov(Hreco, names_reco, ds_reco, 'wt_nominal', 'wt')
+Hreco = fill_hist(Hreco, ds_reco, 'wt_nominal', 'wt')
+covreco = fill_cov(Hreco, ds_reco, 'wt_nominal', 'wt')
 
-Htransfer, names_transfer = build_transfer_hist(
+Htransfer = build_transfer_hist(
     binning_cfg['gen'], binning_cfg['reco']
 )
 ds_transfer = ds.dataset(dspath_transfer, format="parquet")
-Htransfer = fill_hist(Htransfer, names_transfer, ds_transfer, 'wt_nominal', 'wt_reco')
+Htransfer = fill_hist(Htransfer, ds_transfer, 'wt_nominal', 'wt_reco')
