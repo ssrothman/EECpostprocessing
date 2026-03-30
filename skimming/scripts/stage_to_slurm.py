ec5251d81877345fc122b1e361b0a33f650e26e0
Simon Rothman <srothman@mit.edu>
2026-03-30 10:26:06 -0400

    add --exec to stage_to_slurm

--- skimming/scripts/stage_to_slurm.py
+++ skimming/scripts/stage_to_slurm.py
@@ -6,10 +6,21 @@ parser.add_argument("where", type=str, help="Directory of workspace to stage")
 parser.add_argument("name", type=str, help='slurm job name')
 parser.add_argument('--files-per-job', type=int, default=1,
                     help='Number of input files to process per job (default: 1)')
+parser.add_argument('--exec', action='store_true', help='Whether to execute the staging command immediately (default: False)')
 args = parser.parse_args()
 
 from skimming.scaleout.slurm import stage_via_slurm
 import os
 stage_via_slurm(args.where, args.name, args.files_per_job)
-print("Submit with: ")
-print("  sbatch %s"%os.path.join(args.where, "submit_slurm.sh"))
+
+if not args.exec:
+    print("Submit with: ")
+    print("  sbatch %s"%os.path.join(args.where, "submit_slurm.sh"))
+else:
+    cmd = 'sbatch submit_slurm.sh'
+    import subprocess
+    output = subprocess.run(cmd, shell=True, capture_output=True, cwd=args.where)
+    print(output.stdout.decode())
+    print(output.stderr.decode()) 
+    if output.returncode != 0:
+        raise RuntimeError("Failed to submit SLURM jobs")
