c4ae38e39c03168a8b18d1a6d7874f996972c4e1
Dylan Ponman <dponman@umich.edu>
2026-04-09 17:26:25 -0400

    Hardcoding python path

--- skimming/scaleout/templates/condor_exec_template.sh
+++ skimming/scaleout/templates/condor_exec_template.sh
@@ -7,7 +7,7 @@ echo "Starting job $JOB_INDEX"
 echo "Running on host $(hostname)"
 
 source /cvmfs/sft.cern.ch/lcg/views/LCG_106/x86_64-el9-gcc13-opt/setup.sh
-export PYTHONHOME=$(python -c "import sys; print(sys.base_prefix)")
+export PYTHONHOME=/cvmfs/sft.cern.ch/lcg/releases/Python/3.11.9-2924c/x86_64-el9-gcc13-opt
 unset PYTHONPATH
 source /afs/cern.ch/user/d/dponman/EECpostproc/venv/bin/activate
 export PYTHONPATH=/afs/cern.ch/user/d/dponman/EECpostproc:$PYTHONPATH
