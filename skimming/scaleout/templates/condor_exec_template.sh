a78eb9ec899bd2623902d7b7d68fdd74069cc58d
Dylan Ponman <dponman@umich.edu>
2026-04-09 17:38:51 -0400

    Again

--- skimming/scaleout/templates/condor_exec_template.sh
+++ skimming/scaleout/templates/condor_exec_template.sh
@@ -7,9 +7,9 @@ echo "Starting job $JOB_INDEX"
 echo "Running on host $(hostname)"
 
 source /cvmfs/sft.cern.ch/lcg/views/LCG_106/x86_64-el9-gcc13-opt/setup.sh
-export PYTHONHOME=/cvmfs/sft.cern.ch/lcg/releases/Python/3.11.9-2924c/x86_64-el9-gcc13-opt
 unset PYTHONPATH
 source /afs/cern.ch/user/d/dponman/EECpostproc/venv/bin/activate
+export PYTHONHOME=/cvmfs/sft.cern.ch/lcg/releases/Python/3.11.9-2924c/x86_64-el9-gcc13-opt
 export PYTHONPATH=/afs/cern.ch/user/d/dponman/EECpostproc:$PYTHONPATH
 
 cd WORKINGDIR
